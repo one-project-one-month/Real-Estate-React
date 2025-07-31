7084195f15d86746ddf60e31ed82eebecdfff31e
webgipsy <babybird1801@gmail.com>
2025-07-31 17:11:54 +0630

    adding logout function in admin dashboard

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AdminLoginForm } from './components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Dashboard } from './pages';
+import { AuthLoader } from './components/AuthLoader';
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -14,23 +15,27 @@ const App = () => {
           <Route
             path="/"
             element={
-              <Button variant="default" onClick={() => toast.success('Hello!')}>
-                Click me
-              </Button>
-            }
-          />
-          <Route
-            path="/admin/login"
-            element={
               <div className="flex items-center justify-center h-screen">
                 <AdminLoginForm />
               </div>
             }
           />
           <Route
+            path="/admin/login"
+            element={
+              <div className="flex items-center justify-center h-screen">
+                <Button variant="default" onClick={() => toast.success('Hello!')}>
+                  Click me
+                </Button>
+              </div>
+            }
+          />
+          <Route
             path="/admin/dashboard"
             element={
-              <Dashboard />
+              <AuthLoader>
+                <Dashboard />
+              </AuthLoader>
             }
           />
         </Routes>
